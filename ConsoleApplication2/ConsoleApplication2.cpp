128676965edc2fbf48e731d1a01966b911149761
Ba9A-tata <yogadzakkii@gmail.com>
2024-05-05 18:35:01 +0700

    Add project files.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,55 @@
+    1. // int nama; deklarasi variable nama
+      // float tinggi; deklarasi untuk tingii badan
+      // int char; deklrasi untuk data
+
+	2.//if(nBILA == nBILB) {
+    //status = "bilangan A dan bilangan B sama besar";
+    }
+    //else if (nBILA > nBILB) {
+        status = "bilangan A lebih besar dari bilangan B";
+    }
+    //else {
+      //  status = "bilangan A lebih kecil dari bilangan B";
+    }
+    //cout << "Tampilkan status = " << status << endl;
+
+    3. //struct Mahasiswa
+       // string nim;
+       // string nama;
+       // string alamat;
+       // string umur;
+        4.// namaVariabel: = hitung(x, y);
+        5.// int main(){
+            // char pilihan;
+
+//    do {
+//        cout << "Berangkat ambil takjil" << endl;
+//      cout << "Antri Takjil" << endl;
+//      cout << "Ambil Takjil" << endl;
+//      cout << "Apakah mau antri kembali ?" << endl;
+//      cin >> pilihan;
+//    } while (pilihan == 'y' || pilihan == 'Y');
+}
+        6.
+        #Include <iostream>
+        using namespace std;
+        int 20;
+        
+        int main() {
+            string status;
+            int nMTK, nING, nRerata = 0;
+            nRerata; 
+
+            cout << "Masukan nilai MTK =";
+            cin >> nMTK;
+
+            cout << "Masukan nilai ING =";
+            cin >> nING;
+
+            nRerata: nMTK + nING
+            if (nMTK > 80) {
+                status = "Lulus";
+            }
+            else if(nING > 70)
+        }
+
